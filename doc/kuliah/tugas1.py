b006ca12e226f79b1447e563b7439ab9e5a8bd9d
Muhammad Rizky Hasan <harizflame@gmail.com>
2017-03-11 14:21:05 +0700

    Add files via upload

--- doc/kuliah/tugas1.py
+++ doc/kuliah/tugas1.py
@@ -1,1 +1,19 @@
+angka = {
+        'satu' : 1, 'dua' : 2, 'tiga' : 3, 'empat' : 4, 'lima' : 5, 'enam' : 6, 'tujuh' :7, 'delapan' : 8, 'sembilan' : 9}
+
+print("Menghitung Luas Persegi Panjang")
+p = raw_input ('panjang : ')
+l = raw_input ('lebar : ')
+
+if p in angka :
+    p = angka[p]
+
+if l in angka :
+    l = angka[l]
+
+luas = p * l
+
+print ("hasil", luas) 
+
+
 
